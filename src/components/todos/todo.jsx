b44f740829e073b4283777fa291815663b40adcf
prodbygodfather <mahdighasemiabari84@gmail.com>
2025-06-04 10:21:02 +0330

    make todo compnent

---
+++ src/components/todos/todo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './todo.css'
+
+const Todos=(props)=>{
+    return(
+        <>
+            <div className="todos">
+                <p>Number 1</p>
+                <input type="text" class="todo" />
+            </div>
+        </>
+    )
+}
+
+export default Todos
